afd65cf71ef415d2805c3fb823f42a08077cf4b2
yangseungchan <uvzone@naver.com>
2022-11-16 21:02:05 +0900

    [Feature] 프론트엔드 환경 세팅 2차 - mantine, storybook (#21)
    * :sparkles: feature: mantine 설치 및 기본테마 적용
    
    * :sparkles: feature: 외부 폰트 추가 및 eslint quote 설정 변경
    
    * :sparkles: feature: tabler icon 추가
    
    * :sparkles: feature: RouterTransition 효과 추가
    
    * :sparkles: feature: Storybook 도입, StatCounter 컴포넌트
    
    * :goal_net: chore: theme 필요없는 속성 삭제
    
    * :goal_net: chore: StatCounter props 설명 추가
    
    * :goal_net: chore: tsconfig.json 적용범위 수정, main.ts 파일 수정
    
    * :goal_net: chore: eslint 옵션들 세부 조정
    
    type-assertion:off -> stories파일만 적용, no-floating promises warn으로 변경

---
+++ frontend/src/components/StatCounter/index.tsx
@@ -0,0 +1,47 @@
+import styled from '@emotion/styled';
+import { useTheme } from '@emotion/react';
+import { IconHeart, IconBookmark, IconMessageCircle2 } from '@tabler/icons';
+import { Text } from '@mantine/core';
+
+export interface Props {
+  /**
+   * Stat의 종류를 입력합니다.
+   * 각각 좋아요, 댓글, 스크랩을 의미합니다.
+   */
+  variant: 'like' | 'comment' | 'scrap';
+  /**
+   * Stat의 수를 입력합니다.
+   */
+  count: number;
+}
+
+const StatCounter = ({ variant, count = 0 }: Props) => {
+  const {
+    colors: { red, cyan, yellow },
+  } = useTheme();
+  const color = variant === 'like' ? red[7] : variant === 'comment' ? cyan[7] : yellow[7];
+  const Icon =
+    variant === 'like' ? (
+      <IconHeart size={16} color={color} />
+    ) : variant === 'comment' ? (
+      <IconMessageCircle2 size={16} color={color} />
+    ) : (
+      <IconBookmark size={16} color={color} />
+    );
+  return (
+    <StatCounterWrapper>
+      {Icon}
+      <Text fz="md" color={color} weight={500}>
+        {count}
+      </Text>
+    </StatCounterWrapper>
+  );
+};
+
+const StatCounterWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+`;
+
+export default StatCounter;
--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,23 @@
-import '@styles/globals.css';
-import type { AppProps } from 'next/app';
+import { MantineProvider } from '@mantine/core';
+import Head from 'next/head';
+import { AppProps } from 'next/app';
+
+import RouterTransition from '@components/RouterTransition';
+import theme from '@styles/theme';
+import CustomFonts from '@styles/CustomFont';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Page title</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+      <MantineProvider withGlobalStyles withNormalizeCSS theme={theme}>
+        <CustomFonts />
+        <RouterTransition />
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
+  );
 }
