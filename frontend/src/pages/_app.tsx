afc354e49f1b033494604dca7662cf711a98ff95
Yangseungchan <uvzone@naver.com>
2022-11-16 11:52:42 +0900

    :sparkles: feature: mantine 설치 및 기본테마 적용

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@styles/globals.css';
-import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { AppProps } from 'next/app';
+import { MantineProvider } from '@mantine/core';
+import theme from '@styles/theme';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Page title</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+      <MantineProvider withGlobalStyles withNormalizeCSS theme={theme}>
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
+  );
 }
