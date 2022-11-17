b630c4bf54d68f7132fda35a36ed9703cb80692b
myeongil <username1103@naver.com>
2022-11-17 19:09:24 +0900

    :goal_net: chore: 백엔드 개발용 CD github clone script에서 제외

--- backend/scripts/dev-deploy.sh
+++ backend/scripts/dev-deploy.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-rm -rf web13-moyeomoyeo
-
-git clone git@github.com:boostcampwm-2022/web13-moyeomoyeo.git
-
-cd web13-moyeomoyeo/backend
-
 echo $2 | docker login -u $1 $3 --password-stdin
 
 echo "docker logined"
