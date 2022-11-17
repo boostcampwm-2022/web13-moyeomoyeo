747bef24d9c00713d82476d27cc074b9b051a2d0
Gakko <90585081+pythonstrup@users.noreply.github.com>
2022-11-17 17:44:09 +0900

    [Feature] 백엔드 CI (#20)
    * 🥅 chore: docker compose setting
    
    * 🥅 chore: build & test CI
    
    * 🥅 chore: lint test CI
    
    * 🥅 chore: lint test
    
    * 🥅 chore: window eslint setting
    
    * 🥅 chore: ncp docker registry login
    
    * 🥅 chore: ci 오타 수정
    
    * 🥅 chore: docker git commit hash
    
    * 🥅 chore: docker image push
    
    * 🥅 chore: docker push 수정
    
    * :goal_net: chore: 백엔드 개발용 ci docker 이미지화 제거
    
    * :goal_net: chore: 백엔드 코드 변경시에만 ci액션 동작하도록 수정
    
    * :goal_net: chore: .idea, *.iml gitignore 추가
    
    * :goal_net: chore: 백엔드 개발용 ci paths-ignore 제거
    
    Co-authored-by: myeongil <username1103@naver.com>

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -22,5 +22,11 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     'no-console': 'warn',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
   },
 };
