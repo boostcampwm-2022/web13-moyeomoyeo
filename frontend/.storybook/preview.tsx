f03c10f763fe2909cf96bd351ccb8ab623b2a02a
kong430 <alslfkd0430@gmail.com>
2022-11-20 00:03:16 +0900

    :sparkles: feature: globalStyle(fontSize, boxSizing) 적용

--- frontend/.storybook/preview.tsx
+++ frontend/.storybook/preview.tsx
@@ -1,11 +1,15 @@
 import { ReactNode } from 'react';
 import { MantineProvider } from '@mantine/core';
 import theme from '../src/styles/theme';
+import GlobalStyles from '@styles/GlobalStyles';
+import CustomFonts from '@styles/CustomFont';
 
 const ThemeWrapper = (props: { children: ReactNode }) => {
   return (
     <MantineProvider theme={theme} withGlobalStyles withNormalizeCSS>
       {props.children}
+      <CustomFonts />
+      <GlobalStyles />
     </MantineProvider>
   );
 };
--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -3,10 +3,10 @@ import Head from 'next/head';
 import { AppProps } from 'next/app';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-
 import RouterTransition from '@components/RouterTransition';
 import theme from '@styles/theme';
 import CustomFonts from '@styles/CustomFont';
+import GlobalStyles from '@styles/GlobalStyles';
 
 const queryClient = new QueryClient();
 
@@ -19,9 +19,9 @@ export default function App({ Component, pageProps }: AppProps) {
       </Head>
       <QueryClientProvider client={queryClient}>
         <ReactQueryDevtools initialIsOpen={false} />
-
         <MantineProvider withGlobalStyles withNormalizeCSS theme={theme}>
           <CustomFonts />
+          <GlobalStyles />
           <RouterTransition />
           <Component {...pageProps} />
         </MantineProvider>
