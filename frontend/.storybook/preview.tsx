5d8db43de20973e0242315025a131c0877fab82e
Yangseungchan <uvzone@naver.com>
2022-11-20 16:47:00 +0900

    🥅 chore: storybook 기본 패딩 제거

--- frontend/.storybook/preview.tsx
+++ frontend/.storybook/preview.tsx
@@ -5,4 +5,6 @@ const ThemeWrapper = (props: { children: ReactNode }) => {
   return <CommonStyles>{props.children}</CommonStyles>;
 };
 
+export const parameters = { layout: 'fullscreen' };
+
 export const decorators = [(renderStory: Function) => <ThemeWrapper>{renderStory()}</ThemeWrapper>];
