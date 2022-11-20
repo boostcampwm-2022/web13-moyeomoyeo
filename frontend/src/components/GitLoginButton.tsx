b024cf659ece2c6d2f0df41eebfdcf0711121dfa
kong430 <alslfkd0430@gmail.com>
2022-11-21 00:23:38 +0900

    :sparkles: feature: GitLoginButton 컴포넌트 생성

---
+++ frontend/src/components/GitLoginButton.tsx
@@ -0,0 +1,31 @@
+import GithubLogoIcon from '@public/icons/githubLogo.svg';
+import styled from '@emotion/styled';
+
+const GitLoginButton = () => {
+  return (
+    <LoginButton>
+      <GithubLogoIcon />
+      <GithubLoginText>Github로 로그인</GithubLoginText>
+    </LoginButton>
+  );
+};
+
+const LoginButton = styled.button`
+  display: flex;
+  gap: 0.8rem;
+  width: 34.2rem;
+  height: 5.6rem;
+  justify-content: center;
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.dark[9]};
+  border: none;
+  border-radius: 8px;
+`;
+
+const GithubLoginText = styled.span`
+  color: #ffffff;
+  font-size: 1.6rem;
+  font-weight: 800;
+`;
+
+export default GitLoginButton;
