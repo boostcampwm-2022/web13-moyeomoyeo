08ca6895d888da26cd1f6b6ac24dd284b6c64ff5
Yangseungchan <uvzone@naver.com>
2022-11-21 00:10:42 +0900

    ✨ feature: RootTitle 컴포넌트, 스토리 생성

---
+++ frontend/src/components/Header/RootTitle/index.tsx
@@ -0,0 +1,20 @@
+import { Title, Text } from '@mantine/core';
+import { RootTitleWrapper } from './styles';
+
+interface Props {
+  title: string;
+  subTitle: string;
+}
+
+const RootTitle = ({ title, subTitle }: Props) => {
+  return (
+    <RootTitleWrapper>
+      <Title order={3}>{title}</Title>
+      <Text fz="xs" fw={500} c="gray.6">
+        {subTitle}
+      </Text>
+    </RootTitleWrapper>
+  );
+};
+
+export default RootTitle;
