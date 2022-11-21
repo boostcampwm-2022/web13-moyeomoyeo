5f79272ad018128a96bab897cfef7d320d63a356
Yangseungchan <uvzone@naver.com>
2022-11-21 16:16:53 +0900

    🥅 chore: header shadow 적용

--- frontend/src/components/Header/styles.tsx
+++ frontend/src/components/Header/styles.tsx
@@ -7,6 +7,7 @@ const HeaderWrapper = styled.div`
   padding: 0 1.6rem;
   height: 6.4rem;
   background-color: ${({ theme }) => theme.colors.white};
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.1);
 `;
 
 export { HeaderWrapper };
