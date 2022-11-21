166cfc3f59edc157ac474484bbef998cc9f7f3d0
myeongil <username1103@naver.com>
2022-11-22 01:07:35 +0900

    :sparkles: feature: swagger 셋업
    - @nestjs/swagger 설치
    - swagger 셋업
      - API 메소드 순으로 정렬
      - 요청 목록 서버에 localhost 추가
      - /v1/docs에 swagger 페이지 추가

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,11 +3,14 @@ import { AppModule } from './app.module';
 import { AppConfigService } from './common/config/app/config.service';
 
 import { setNestApp } from './setNestApp';
+import { setSwagger } from './setSwagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   setNestApp(app);
+
+  setSwagger(app);
 
   const appConfigService = app.get(AppConfigService);
   await app.listen(appConfigService.port);
---
+++ backend/src/setSwagger.ts
@@ -0,0 +1,43 @@
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppConfigService } from './common/config/app/config.service';
+
+export const setSwagger = (app: INestApplication) => {
+  const appConfigService = app.get(AppConfigService);
+
+  const config = new DocumentBuilder()
+    .setTitle('MoyeoMoyeo REST API Specification')
+    .setVersion('1.0.0')
+    .addServer(`http://localhost:${appConfigService.port}`)
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config, {
+    include: [],
+    extraModels: [],
+  });
+
+  SwaggerModule.setup('/v1/docs', app, document, {
+    swaggerOptions: {
+      operationsSorter: (a: any, b: any) => {
+        const methodsOrder = [
+          'post',
+          'put',
+          'patch',
+          'get',
+          'delete',
+          'options',
+          'trace',
+        ];
+        let result =
+          methodsOrder.indexOf(a.get('method')) -
+          methodsOrder.indexOf(b.get('method'));
+
+        if (result === 0) {
+          result = a.get('path').localeCompare(b.get('path'));
+        }
+
+        return result;
+      },
+    },
+  });
+};
