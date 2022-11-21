edfdce354aed1e01bb29589265ca4b61ead9f05f
myeongil <username1103@naver.com>
2022-11-21 15:29:57 +0900

    :sparkles: feature: 서버 실행 포트를 환경변수에서 가져오도록 수정

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AppConfigModule } from './common/config/app/config.module';
 
 @Module({
-  imports: [],
+  imports: [AppConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AppConfigService } from './common/config/app/config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const appConfigService = app.get(AppConfigService);
+  await app.listen(appConfigService.port);
 }
 
 bootstrap();
