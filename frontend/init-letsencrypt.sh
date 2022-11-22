1f84b509a34393415ed6cffd95f1ff632c080c0b
Gakko <pythonstrup@gmail.com>
2022-11-22 18:40:45 +0900

    🥅 chore: next cache를 위한 nginx 설정

--- frontend/init-letsencrypt.sh
+++ frontend/init-letsencrypt.sh
@@ -8,7 +8,7 @@ fi
 
 domains=(www.moyeomoyeo.com)
 rsa_key_size=4096
-data_path="~/dev/certbot"
+data_path="/letsencrypt/certbot"
 email="pythonstrup@gmail.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
