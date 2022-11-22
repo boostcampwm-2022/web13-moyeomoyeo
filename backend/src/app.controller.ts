6fbcec1171e2058b9774c5b4ca0e5897acb1ef2f
myeongil <username1103@naver.com>
2022-11-22 10:44:34 +0900

    :sparkles: feature: ApiSuccessReponse 데코레이터 추가
    - ResponseEntity에 맞는 적절한 swagger 변환을 위해 추가

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpStatus } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ResponseEntity } from './common/response-entity';
+import { ApiTags } from '@nestjs/swagger';
+import { ApiSuccessResponse } from './common/decorator/api-success-resposne.decorator';
 
 @Controller()
+@ApiTags('example')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiSuccessResponse(HttpStatus.OK)
   getHello() {
     return ResponseEntity.OK_WITH_DATA(this.appService.getHello());
   }
