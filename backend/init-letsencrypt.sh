9ade6394d4a9318dd8eb9dd1352a73d7ef9986b1
Gakko <pythonstrup@gmail.com>
2022-11-22 11:29:48 +0900

    🥅 chore: init script 변경

--- backend/init-letsencrypt.sh
+++ backend/init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(api.moyeomoyeo.com)
+domains=(*.moyeomoyeo.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="pythonstrup@gmail.com" # Adding a valid address is strongly recommended
