ec6f9568c47cb700d6e5bc6cc12e3362eed42d09
kong430 <alslfkd0430@gmail.com>
2022-11-23 17:33:26 +0900

    :sparkles: feature: ArticleTag 컴포넌트에서 color를 props로 받도록 변경

--- frontend/src/components/ArticleTag/index.tsx
+++ frontend/src/components/ArticleTag/index.tsx
@@ -1,29 +1,21 @@
 import { Badge } from '@mantine/core';
 import styled from '@emotion/styled';
-import { BADGE_COLORS } from '@constants/color';
 
 interface Props {
   /**
-   * 카테고리 id, 지역 id 등 뱃지 옵션의 id를 입력합니다.
    * id를 통해 색상이 결정됩니다.
    */
-  id: number;
+  color: string;
   /**
    * 뱃지에 들어갈 내용이 입력됩니다.
    */
   content: string;
 }
 
-const ArticleTag = ({ id, content }: Props) => {
-  // TODO 이 함수를 util로 분리하는게 좋을지 고민
-  const getBadgeColor = (id: number) => {
-    const colorCount = BADGE_COLORS.length;
-    return BADGE_COLORS[id % colorCount];
-  };
-
+const ArticleTag = ({ color, content }: Props) => {
   return (
     <BadgeWrapper>
-      <Badge color={getBadgeColor(id)} size="sm">
+      <Badge color={color} size="sm">
         {content}
       </Badge>
     </BadgeWrapper>
