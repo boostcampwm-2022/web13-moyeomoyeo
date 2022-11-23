718af418e7ecbbf3b66eba2c6af849b84ffcf3a3
kong430 <alslfkd0430@gmail.com>
2022-11-23 14:40:40 +0900

    :sparkles: feature: msw url 변경

--- frontend/src/mocks/handlers.ts
+++ frontend/src/mocks/handlers.ts
@@ -1,4 +1,4 @@
 import { rest } from 'msw';
 import { getTest } from './resolvers/test';
 
-export const handlers = [rest.get('http://localhost:4000/test', getTest)];
+export const handlers = [rest.get('http://testServer/test', getTest)];
