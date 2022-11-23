4cb7d41c05ca06f11ce52e01a28cf400d929104a
Gakko <pythonstrup@gmail.com>
2022-11-23 15:05:51 +0900

    ✨ feature: s3 config validate 작성

---
+++ backend/src/common/config/s3/validate.ts
@@ -0,0 +1,47 @@
+import { IsString, IsUrl, validateSync } from 'class-validator';
+import { Expose, plainToClass, Type } from 'class-transformer';
+
+export class S3Config {
+  @IsString()
+  @Type(() => String)
+  @Expose()
+  STORAGE_ACCESSKEY: string;
+
+  @IsString()
+  @Type(() => String)
+  @Expose()
+  STORAGE_SECRETKEY: string;
+
+  @IsString()
+  @Type(() => String)
+  @Expose()
+  STORAGE_REGION: string;
+
+  @IsUrl()
+  @Type(() => String)
+  @Expose()
+  STORAGE_ENDPOINT: string;
+
+  @IsString()
+  @Type(() => String)
+  @Expose()
+  STORAGE_BUCKET: string;
+}
+
+export const validate = (config: Record<string, unknown>) => {
+  const validatedConfig = plainToClass(
+    S3Config,
+    { ...config },
+    { enableImplicitConversion: true, excludeExtraneousValues: true },
+  );
+
+  const errors = validateSync(validatedConfig, {
+    skipMissingProperties: false,
+  });
+
+  if (errors.length > 0) {
+    throw new Error(errors.toString());
+  }
+
+  return validatedConfig;
+};
