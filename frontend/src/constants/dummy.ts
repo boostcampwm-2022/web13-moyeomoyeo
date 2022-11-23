4f13e4b40b98fe64e3f39e1004beabc2c5920e7a
kong430 <alslfkd0430@gmail.com>
2022-11-23 17:31:59 +0900

    :sparkles: feature: category enum으로 정의

--- frontend/src/constants/dummy.ts
+++ frontend/src/constants/dummy.ts
@@ -1,9 +1,10 @@
-import { ArticleType } from '@typings/types';
+import { ArticlePreviewType, ArticleType } from '@typings/types';
 import { Location } from '@constants/location';
 import { ArticleStatus } from '@constants/article';
+import { Category } from '@constants/category';
 
 // TODO 개발용 dummy data, 추후 삭제
-const dummyArticle: ArticleType = {
+const dummyArticlePreview: ArticlePreviewType = {
   id: 1,
   title: '모집 테스트1',
   location: {
@@ -12,7 +13,7 @@ const dummyArticle: ArticleType = {
   },
   category: {
     id: 1,
-    name: '스터디',
+    name: Category.STUDY,
   },
   commentCount: 31,
   scrapCount: 12,
@@ -23,4 +24,28 @@ const dummyArticle: ArticleType = {
   createdAt: '2021-04-03T18:45:19Z',
 };
 
-export { dummyArticle };
+const dummyArticle: ArticleType = {
+  id: 1,
+  title: '모집 테스트1',
+  location: {
+    id: 2,
+    name: Location.BUSAN,
+  },
+  category: {
+    id: 1,
+    name: Category.STUDY,
+  },
+  contents: '<div>test</div>',
+  authorId: 1,
+  authorName: 'JHPark_GOD',
+  authorThumbnail: 'https://avatars.githubusercontent.com/u/90585081?v=4',
+  commentCount: 31,
+  scrapCount: 12,
+  thumbnail: 'https://avatars.githubusercontent.com/u/90585081?v=4',
+  maxCapacity: 5,
+  currentCapacity: 3,
+  status: ArticleStatus.PROGRESS,
+  createdAt: '2021-04-03T18:45:19Z',
+};
+
+export { dummyArticlePreview, dummyArticle };
