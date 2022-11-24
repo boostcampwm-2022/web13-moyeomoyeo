9277f0554ee1da01583eed0e78933957324eaa8a
pythonstrup <pythonstrup@gmail.com>
2022-11-24 17:11:37 +0900

    ✨ feature: image upload response dto 작성

---
+++ backend/src/image/dto/images-upload-response.dto.ts
@@ -0,0 +1,23 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class ImagesUploadResponse {
+  @ApiProperty({
+    example: '1669276833875-64adca9c-94cd-4162-a53f-f75e951e39db',
+    description: '이미지 key',
+    required: true,
+  })
+  key: string;
+
+  @ApiProperty({
+    example:
+      'https://kr.object.ncloudstorage.com/uploads/images/1669276833875-64adca9c-94cd-4162-a53f-f75e951e39db',
+    description: '버킷 이미지 url',
+    required: true,
+  })
+  url: string;
+
+  constructor(key: string, url: string) {
+    this.key = key;
+    this.url = url;
+  }
+}
