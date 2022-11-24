c8c42f0824a7f90196b1c25b2462f7903899a267
Yangseungchan <uvzone@naver.com>
2022-11-24 16:34:12 +0900

    ✨ feature: 에디터 컴포넌트 추가

---
+++ frontend/src/components/common/ArticleEditor/index.tsx
@@ -0,0 +1,46 @@
+import dynamic from 'next/dynamic';
+import { Text } from '@mantine/core';
+
+import styled from '@emotion/styled';
+
+const RichTextEditor = dynamic(() => import('@mantine/rte'), {
+  ssr: false,
+  loading: () => null,
+});
+
+const ArticleEditor = () => {
+  return (
+    <ArticleEditorWrapper>
+      <ArticleEditorLabel>
+        <Text size="md" fw={500}>
+          내용
+        </Text>
+        <Text c="red" size="md">
+          *
+        </Text>
+      </ArticleEditorLabel>
+      <StyledEditor id="rte" />
+    </ArticleEditorWrapper>
+  );
+};
+
+const ArticleEditorWrapper = styled.div``;
+
+const ArticleEditorLabel = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.4rem;
+  margin-bottom: 0.4rem;
+`;
+
+const StyledEditor = styled(RichTextEditor)`
+  & .quill,
+  & .ql-container {
+    min-height: 40rem;
+    max-height: 40rem;
+    overflow: auto;
+    font-size: 1.4rem;
+  }
+`;
+
+export default ArticleEditor;
