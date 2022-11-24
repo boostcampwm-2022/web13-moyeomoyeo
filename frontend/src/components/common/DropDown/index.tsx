1e13c82e6827175866e2230d4688c5964c3e99b7
Yangseungchan <uvzone@naver.com>
2022-11-24 15:24:25 +0900

    ✨ feature: Dropdown 컴포넌트 추가

---
+++ frontend/src/components/common/DropDown/index.tsx
@@ -0,0 +1,27 @@
+import styled from '@emotion/styled';
+import { Select, SelectProps } from '@mantine/core';
+
+interface Props extends SelectProps {}
+
+const DropDown = (props: Props) => {
+  return <StyledSelect size="md" {...props} />;
+};
+
+const StyledSelect = styled(Select)`
+  width: 100%;
+  & .mantine-Select-item {
+    padding: 1.2rem 1.6rem;
+    &[data-selected] {
+      &,
+      &:hover {
+        background-color: ${({ theme }) => theme.colors.indigo[0]};
+        color: ${({ theme }) => theme.colors.indigo[7]};
+      }
+    }
+  }
+  & .mantine-Select-label {
+    padding-bottom: 0.4rem;
+  }
+`;
+
+export default DropDown;
