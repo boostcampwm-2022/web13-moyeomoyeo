833fd05340d167e8e9c47798fd82e184d308a678
Yangseungchan <uvzone@naver.com>
2022-11-24 12:26:38 +0900

    🥅 chore: timeago import 경로 수정

--- frontend/src/utils/dateTime.ts
+++ frontend/src/utils/dateTime.ts
@@ -1,9 +1,8 @@
-import * as timeago from 'timeago.js';
-
+import { register, format } from 'timeago.js';
 import ko from 'timeago.js/lib/lang/ko';
 
-timeago.register('ko', ko);
+register('ko', ko);
 
 export default function dateTimeFormat(date: string | Date) {
-  return timeago.format(date, 'ko');
+  return format(date, 'ko');
 }
