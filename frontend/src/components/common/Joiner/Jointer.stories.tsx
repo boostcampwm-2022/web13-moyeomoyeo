8454cb0dcdb807376f1aa08392bd2b6e2c137fc2
kong430 <alslfkd0430@gmail.com>
2022-11-26 12:50:04 +0900

    :sparkles: feature: Jointer 스토리 생성

---
+++ frontend/src/components/common/Joiner/Jointer.stories.tsx
@@ -0,0 +1,14 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import Joiner from '.';
+
+export default {
+  title: 'Component/Joiner',
+  component: Joiner,
+} as ComponentMeta<typeof Joiner>;
+
+const Template: ComponentStory<typeof Joiner> = (args) => <Joiner {...args} />;
+
+export const _Joiner = Template.bind({});
+_Joiner.args = {
+  components: [<div key={1}>test1</div>, <div key={2}>test2</div>],
+};
