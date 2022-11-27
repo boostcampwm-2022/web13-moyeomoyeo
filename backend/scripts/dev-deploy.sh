e7ae56a6fd9325df8ca0be34b69b8aaa9a25f86c
Kim Myeongil <67570061+username1103@users.noreply.github.com>
2022-11-27 22:20:07 +0900

    [FIX] 백엔드 CD 수정 (#91)
    * :goal_net: chore: 백엔드 cd 안쓰는 설정 제거
    
    * :goal_net: chore: 백엔드 cd 환경변수 세팅
    
    * :goal_net: chore: 개발용 스크립트 수정
    
    - docker compose down env file 설정
    - 컨테이너 전체 삭제 스크립트 제거
    
    * :goal_net: chore: env file 설정 문법 수정
    
    * :goal_net: chore: docker compose down 수정
    
    * :goal_net: chore: nginx ssl 옵션 끄기
    
    * :goal_net: chore: 백엔드 cd script 수정

--- backend/scripts/dev-deploy.sh
+++ backend/scripts/dev-deploy.sh
@@ -4,19 +4,16 @@ echo $2 | docker login -u $1 $3 --password-stdin
 
 echo "docker logined"
 
-cd backend
+cd backend || exit 1
 
-touch .env.development
+touch .env
 
-echo -e $4 > .env.development
+echo -e $4 > .env
+
+echo "create .env"
 
 # docker down
-docker compose down
+docker compose down --rmi --remove-orphans
 
-# 도커 컨테이너 전체 삭제
-docker rm `docker ps -a -q`
-
-# 도커 이미지 전체 삭제
-docker rmi `docker images -q`
-
-docker compose --env-file .env.development up -d --build
+# docker up
+docker compose up -d --build
