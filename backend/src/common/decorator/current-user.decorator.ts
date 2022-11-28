269b3072f84ab5b57068349e735a89c2bfb4db1d
pythonstrup <pythonstrup@gmail.com>
2022-11-28 23:15:22 +0900

    🥅 chore: 데코레이터 방식 변경

--- backend/src/common/decorator/current-user.decorator.ts
+++ backend/src/common/decorator/current-user.decorator.ts
@@ -1,9 +1,8 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { GqlExecutionContext } from '@nestjs/graphql';
 
 export const CurrentUser = createParamDecorator(
   (data: unknown, context: ExecutionContext) => {
-    const ctx = GqlExecutionContext.create(context);
-    return ctx.getContext().req.user;
+    const request = context.switchToHttp().getRequest();
+    return request.user;
   },
 );
