b533d886b55a7c6bcefc20897506b5da0bf024aa
pythonstrup <pythonstrup@gmail.com>
2022-11-28 11:59:26 +0900

    🥅 chore: constants 파일명 변경

---
+++ backend/src/app/group-article/constants/group-article.constants.ts
@@ -0,0 +1,46 @@
+export enum CATEGORY {
+  MEAL = '식사',
+  STUDY = '스터디',
+  ETC = '기타',
+  COMPITITION = '공모전',
+  PROJECT = '프로젝트',
+}
+
+export enum LOCATION {
+  ONLINE = '온라인',
+  SEOUL = '서울',
+  INCHEON = '인천',
+  BUSAN = '부산',
+  DAEGU = '대구',
+  GWANGJU = '광주',
+  DAEJEON = '대전',
+  ULSAN = '울산',
+  SEJONG = '세종',
+  GYEONGGI = '경기',
+  GANGWON = '강원',
+  CHUNGBUK = '충북',
+  CHUNGNAM = '충남',
+  JEONBUK = '전북',
+  JEONNAM = '전남',
+  GYEONGBUK = '경북',
+  GYEONGNAM = '경남',
+  JEJU = '제주',
+}
+
+export enum GROUP_APPLICATION_STATUS {
+  REGISTER,
+  CANCEL,
+}
+
+export enum GROUP_STATUS {
+  STILL = '모집중',
+  SUCCESS = '모집완료',
+  CANCEL = '모집중단',
+}
+
+export enum ARTICLE {
+  GROUP = 'GROUP',
+  FREE = 'FREE',
+  QUESTION = 'QUESTION',
+  PROJECT = 'PROJECT',
+}
