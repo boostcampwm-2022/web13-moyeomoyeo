12ad843d9d7c4c8400dc38a06e561585c61b4eeb
pythonstrup <pythonstrup@gmail.com>
2022-11-29 01:26:10 +0900

    🥅 chore: 코드 리뷰 내용 반영
    - 불필요한 유효성 검증 제거

--- backend/src/app/myinfo/myinfo.controller.ts
+++ backend/src/app/myinfo/myinfo.controller.ts
@@ -7,9 +7,6 @@ import { MyInfoGetResponse } from '@app/myinfo/dto/myinfo-get-response.dto';
 import { ResponseEntity } from '@src/common/response-entity';
 import { CurrentUser } from '@src/common/decorator/current-user.decorator';
 import { User } from '@app/user/entity/user.entity';
-import { ApiErrorResponse } from '@src/common/decorator/api-error-response.decorator';
-import { UserIdNotFoundException } from '@app/myinfo/exception/id-not-found.exception';
-import { UsernameNotFoundException } from '@app/myinfo/exception/username-not-found.exception';
 
 @Controller('/my-info')
 @JwtAuth()
@@ -19,10 +16,8 @@ export class MyInfoController {
 
   @Get()
   @ApiSuccessResponse(HttpStatus.OK, MyInfoGetResponse)
-  @ApiErrorResponse(UserIdNotFoundException, UsernameNotFoundException)
   async getMyInfo(@CurrentUser() user: User) {
     const data = MyInfoGetResponse.from(user);
-    await this.myInfoService.checkUserInfo(user);
     return ResponseEntity.OK_WITH_DATA(data);
   }
 
