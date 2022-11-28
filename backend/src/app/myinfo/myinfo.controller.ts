e2b0a1c5b483d6083edcea911c71c680e546cca4
pythonstrup <pythonstrup@gmail.com>
2022-11-29 00:49:45 +0900

    🥅 chore: 코드 리뷰 반영

--- backend/src/app/myinfo/myinfo.controller.ts
+++ backend/src/app/myinfo/myinfo.controller.ts
@@ -3,7 +3,7 @@ import { JwtAuth } from '@src/common/decorator/jwt-auth.decorator';
 import { MyInfoService } from '@app/myinfo/myinfo.service';
 import { ApiTags } from '@nestjs/swagger';
 import { ApiSuccessResponse } from '@src/common/decorator/api-success-resposne.decorator';
-import { MyInfoGetResponse } from './dto/myinfo-get-response.dto';
+import { MyInfoGetResponse } from '@app/myinfo/dto/myinfo-get-response.dto';
 import { ResponseEntity } from '@src/common/response-entity';
 import { CurrentUser } from '@src/common/decorator/current-user.decorator';
 import { User } from '@app/user/entity/user.entity';
