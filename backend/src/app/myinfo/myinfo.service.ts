a547ad15cb9049ecf6bca6021eb7f980728bd457
pythonstrup <pythonstrup@gmail.com>
2022-11-28 23:17:00 +0900

    ✨ feature: get myinfo 유저 유효성 검증
    - DB 내용과 비교

--- backend/src/app/myinfo/myinfo.service.ts
+++ backend/src/app/myinfo/myinfo.service.ts
@@ -1,7 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { UserRepository } from '@app/user/user.repository';
+import { User } from '@app/user/entity/user.entity';
+import { UserIdNotFoundException } from './exception/id-not-found.exception';
+import { UsernameNotFoundException } from './exception/username-not-found.exception';
 
 @Injectable()
 export class MyInfoService {
   constructor(private readonly userRepository: UserRepository) {}
+
+  async checkUserInfo(user: User) {
+    const userItem = await this.userRepository.findById(user.id);
+    if (!userItem) {
+      throw new UserIdNotFoundException();
+    }
+
+    if (userItem.username !== user.username) {
+      throw new UsernameNotFoundException();
+    }
+  }
 }
