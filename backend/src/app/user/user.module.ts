77457871ee237c9945e009c3a57334532af609a4
pythonstrup <pythonstrup@gmail.com>
2022-11-29 00:16:30 +0900

    ✨ feature: 모듈 및 스웨거 적용

--- backend/src/app/user/user.module.ts
+++ backend/src/app/user/user.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UserRepository } from '@app/user/user.repository';
+import { UserService } from '@app/user/user.service';
+import { UserController } from '@app/user/user.controller';
+import { JwtTokenModule } from '@src/common/module/jwt-token/jwt-token.module';
 
 @Module({
-  providers: [UserRepository],
+  imports: [JwtTokenModule],
+  controllers: [UserController],
+  providers: [UserService, UserRepository],
   exports: [UserRepository],
 })
 export class UserModule {}
