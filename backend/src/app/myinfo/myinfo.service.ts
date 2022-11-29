bd5a69b7b5330f16907d6bbb6fdcd444792632d3
pythonstrup <pythonstrup@gmail.com>
2022-11-29 16:52:37 +0900

    ✨ feature: 프로필 수정 로직 및 예외처리

--- backend/src/app/myinfo/myinfo.service.ts
+++ backend/src/app/myinfo/myinfo.service.ts
@@ -1,7 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import { UserRepository } from '@app/user/user.repository';
+import { ProfileModifyingRequest } from '@app/myinfo/dto/profile-modifying-request.dto';
+import { UserNameDuplicateException } from './exception/username-duplicate.exception';
 
 @Injectable()
 export class MyInfoService {
   constructor(private readonly userRepository: UserRepository) {}
+
+  async modifyProfile(
+    userId: number,
+    userName: string,
+    profileModifyingRequest: ProfileModifyingRequest,
+  ) {
+    const user = await this.userRepository.findByUsername(
+      profileModifyingRequest.userName,
+    );
+    if (user && userName !== profileModifyingRequest.userName) {
+      throw new UserNameDuplicateException();
+    }
+
+    this.userRepository.updateUser({
+      id: userId,
+      userName: profileModifyingRequest.userName,
+      profileImage: profileModifyingRequest.profileImage,
+      description: profileModifyingRequest.description,
+      githubUrl: profileModifyingRequest.githubUrl,
+      blogUrl: profileModifyingRequest.blogUrl,
+    });
+  }
 }
