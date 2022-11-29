ce7c9071b8f2cc6501d1abe8c9dabd0fda4ce6eb
Yangseungchan <uvzone@naver.com>
2022-11-29 18:23:40 +0900

    🥅 chore: 스토리북 queryclient 로드 안되는 문제 해결

--- frontend/.storybook/preview.tsx
+++ frontend/.storybook/preview.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from 'react';
 import { RouterContext } from 'next/dist/shared/lib/router-context';
 import CommonStyles from '@styles/CommonStyles';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const ThemeWrapper = (props: { children: ReactNode }) => {
   return <CommonStyles>{props.children}</CommonStyles>;
@@ -13,4 +14,12 @@ export const parameters = {
   },
 };
 
-export const decorators = [(renderStory: Function) => <ThemeWrapper>{renderStory()}</ThemeWrapper>];
+const queryClient = new QueryClient();
+
+export const decorators = [
+  (renderStory: Function) => (
+    <QueryClientProvider client={queryClient}>
+      <ThemeWrapper>{renderStory()}</ThemeWrapper>
+    </QueryClientProvider>
+  ),
+];
