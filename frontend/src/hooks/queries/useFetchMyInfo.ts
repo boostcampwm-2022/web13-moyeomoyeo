df1b0e7e9a68d7ee4567ad363819d70fdcffa09e
Yangseungchan <uvzone@naver.com>
2022-11-29 16:36:31 +0900

    ♻️ refactor: useFetchMyInfo에 리다이렉션 로직 추가

--- frontend/src/hooks/queries/useFetchMyInfo.ts
+++ frontend/src/hooks/queries/useFetchMyInfo.ts
@@ -1,19 +1,36 @@
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
 import { useQuery } from '@tanstack/react-query';
 import axios, { AxiosError } from 'axios';
 
 import { ApiResponse, UserType } from '@typings/types';
 
-const useFetchMyInfo = () => {
-  const { data, isLoading } = useQuery<ApiResponse<UserType>, AxiosError, UserType>(
+/**
+ * 로그인 시 유저정보를 반환하며
+ * redirect = true 인 경우 로그인 페이지로 리다이렉션 처리
+ */
+
+const useFetchMyInfo = (redirect = false) => {
+  const router = useRouter();
+  const { data, isLoading, isError } = useQuery<ApiResponse<UserType>, AxiosError, UserType>(
     ['my'],
     () => axios.get('/api/v1/my-info'),
     {
       select: (res) => res.data.data,
-      retry: 0,
-      staleTime: 1000 * 60 * 5,
+      retry: false,
+      staleTime: 1000 * 60 * 4,
     }
   );
-  return { data, isLoading };
+
+  useEffect(() => {
+    if (redirect && !isLoading && !data) {
+      void router.push('/login');
+      alert('로그인이 필요합니다.');
+    }
+  }, [redirect, data, isLoading]);
+
+  return { data, isLoading, isError };
 };
 
 export default useFetchMyInfo;
