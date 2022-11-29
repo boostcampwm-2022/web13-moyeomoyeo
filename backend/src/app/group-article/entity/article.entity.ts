6b4eda5b3cfa40050e0917fb152d404fa8492a1b
myeongil <username1103@naver.com>
2022-11-29 14:56:26 +0900

    :sparkles: feature: Article엔티티에 유저 연관관계 추가

--- backend/src/app/group-article/entity/article.entity.ts
+++ backend/src/app/group-article/entity/article.entity.ts
@@ -2,16 +2,26 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  JoinColumn,
+  ManyToOne,
   PrimaryGeneratedColumn,
   TableInheritance,
   UpdateDateColumn,
 } from 'typeorm';
+import { User } from '@app/user/entity/user.entity';
 
 @Entity({ name: 'article' })
 @TableInheritance({ pattern: 'STI', column: { type: 'varchar', name: 'type' } })
 export abstract class Article {
   @PrimaryGeneratedColumn({ unsigned: true })
   id: number;
+
+  @Column({ unsigned: true })
+  userId: number;
+
+  @ManyToOne(() => User, { lazy: true, nullable: false })
+  @JoinColumn({ referencedColumnName: 'id', name: 'user_id' })
+  user: Promise<User>;
 
   @Column({ type: 'varchar', length: 100 })
   title: string;
