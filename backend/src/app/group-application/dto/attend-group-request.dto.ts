138e1fbe137a55e27c57bab2643af855ca2e76ea
pythonstrup <pythonstrup@gmail.com>
2022-11-29 22:48:23 +0900

    ✨ feature: 모집 신청하기 request dto 작성

---
+++ backend/src/app/group-application/dto/attend-group-request.dto.ts
@@ -0,0 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber } from 'class-validator';
+
+export class AttendGroupRequest {
+  @ApiProperty({
+    example: 1,
+    description: '그룹 아이디',
+    required: true,
+  })
+  @IsNumber()
+  groupId: number;
+}
