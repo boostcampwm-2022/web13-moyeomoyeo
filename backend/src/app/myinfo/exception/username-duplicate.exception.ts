c71108b7fa5061d6a312cb737a024fd388ba524a
pythonstrup <pythonstrup@gmail.com>
2022-11-29 16:50:12 +0900

    ✨ feature: 유저이름 중복 exception 작성

---
+++ backend/src/app/myinfo/exception/username-duplicate.exception.ts
@@ -0,0 +1,9 @@
+import { BadRequestException } from '@nestjs/common';
+
+export class UserNameDuplicateException extends BadRequestException {
+  constructor(
+    message = '중복된 유저이름으로 요청했습니다! 해당 유저이름으로 바꿀 수 없습니다.',
+  ) {
+    super({ status: 'USER_NAME_BAD_REQUEST', message });
+  }
+}
