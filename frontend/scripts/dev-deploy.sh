590e3d40b52be10060963ee75f8b5d99553f2f06
yangseungchan <uvzone@naver.com>
2022-11-30 14:33:53 +0900

    [Feature] Dockerfile에 환경 변수 주입 (#145)
    * :bug: fix: Dockerfile에 환경 변수 주입
    
    * 🥅 chore: cd 테스트
    
    * 🥅 chore: cd 테스트
    
    * 🥅 chore: build-args 값 수정
    
    * 🥅 chore: cd에서 환경변수 잘 나오는지 테스트
    
    * 🥅 chore: Dockerfile 수정
    
    * 🥅 chore: Dockerfile 수정 및 테스트
    
    * 🥅 chore: docker file 수정
    
    * 🥅 chore: 도커파일 재수정
    
    * 🥅 chore: Dockerfile 수정
    
    * 🥅 chore: ci cd 스크립트들 수정

--- frontend/scripts/dev-deploy.sh
+++ frontend/scripts/dev-deploy.sh
@@ -7,8 +7,11 @@ echo "docker has been logged in"
 cd frontend
 
 touch .env
+touch .env.production
 
-echo -e $4 > .env
+echo -e $4 > .env.production
+
+echo -e API_URL=https://api.moyeomoyeo.com > .env
 
 # docker down
 docker compose down --rmi all --remove-orphans
