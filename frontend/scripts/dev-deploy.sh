5a2d6b24e33a011c7f36813076967f700d8609e5
Yangseungchan <uvzone@naver.com>
2022-11-30 15:58:13 +0900

    🥅 chore: 컨테이너 구성 스크립트 수정

--- frontend/scripts/dev-deploy.sh
+++ frontend/scripts/dev-deploy.sh
@@ -7,13 +7,8 @@ echo "docker has been logged in"
 cd frontend
 
 touch .env
-touch .env.production
 
 echo -e $4 > .env
-
-echo -e API_URL=https://api.moyeomoyeo.com\\nNEXT_PUBLIC_API_URL=https://api.moyeomoyeo.com > .env.production
-
-cat .env.production
 
 # docker down
 docker compose down --rmi all --remove-orphans
