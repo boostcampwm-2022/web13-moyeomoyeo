af09480b6b730f21bc729df7e35e8cfe2e71e5f3
pythonstrup <pythonstrup@gmail.com>
2022-11-30 10:51:31 +0900

    🥅  chore: 코드리뷰 반영

--- backend/src/app/group-application/exception/cannot-applicate.exception.ts
+++ backend/src/app/group-application/exception/cannot-applicate.exception.ts
@@ -1,7 +1,7 @@
 import { BadRequestException } from '@nestjs/common';
 
 export class CannotApplicateException extends BadRequestException {
-  constructor(message = '당신이 만든 그룹에 신청을 할 수 없습니다.!') {
+  constructor(message = '당신이 만든 그룹에 신청을 할 수 없습니다.') {
     super({ status: 'CAN_NOT_APPLICATE_BAD_REQUEST', message });
   }
 }
