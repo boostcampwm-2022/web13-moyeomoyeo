7ce7e1ef6c40fd66fae2633cc8ad5fe68792f497
kong430 <alslfkd0430@gmail.com>
2022-12-01 23:02:29 +0900

    :sparkles: feature: 채팅방 링크 복사 완료 시 toast message show

--- frontend/src/components/article/ParticipateButton/ChatLinkButton/index.tsx
+++ frontend/src/components/article/ParticipateButton/ChatLinkButton/index.tsx
@@ -1,6 +1,9 @@
-import { Button } from '@mantine/core';
+import { useCallback, useEffect } from 'react';
 
-import AlertModal from '@components/common/AlertModal';
+import { Button } from '@mantine/core';
+import { showNotification } from '@mantine/notifications';
+import { IconCheck } from '@tabler/icons';
+
 import useClipboard from '@hooks/useClipboard';
 
 interface Props {
@@ -10,18 +13,36 @@ interface Props {
 const ChatLinkButton = ({ chatLink }: Props) => {
   const { isCopied, setIsCopied, doCopy } = useClipboard();
 
+  const showSuccessToast = useCallback(() => {
+    showNotification({
+      color: 'indigo',
+      title: '오픈 채팅방 링크 복사 완료!',
+      message: '지금 바로 채팅에 참여해보세요.',
+      icon: <IconCheck size={16} />,
+      autoClose: 4000,
+      styles: (theme) => ({
+        root: {
+          paddingTop: '1.6rem',
+          paddingBottom: '1.6rem',
+        },
+        title: {
+          fontSize: theme.fontSizes.lg,
+          fontWeight: 700,
+        },
+      }),
+    });
+    setIsCopied(false);
+  }, [setIsCopied]);
+
+  useEffect(() => {
+    if (isCopied) showSuccessToast();
+  }, [isCopied, showSuccessToast]);
+
   return (
     <>
       <Button onClick={() => doCopy(chatLink)} size="md" color="indigo" fullWidth>
         오픈채팅방 링크 복사
       </Button>
-      {isCopied && (
-        <AlertModal
-          message="클립보드에 복사되었습니다"
-          open={isCopied}
-          onClose={() => setIsCopied(false)}
-        />
-      )}
     </>
   );
 };
