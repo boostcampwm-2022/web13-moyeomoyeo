8949aebbd6fa1695b7dec7031f9eb617406da5fc
kong430 <alslfkd0430@gmail.com>
2022-12-01 22:40:52 +0900

    :bug: fix: isJoined 여부 받아오는 API url 수정

--- frontend/src/hooks/queries/useFetchApplicationStatus.ts
+++ frontend/src/hooks/queries/useFetchApplicationStatus.ts
@@ -4,10 +4,14 @@ import useAuthQuery from '@hooks/useAuthQuery';
 import { ApiResponse } from '@typings/types';
 import { clientAxios } from '@utils/commonAxios';
 
+interface DataType {
+  isJoined: boolean;
+}
+
 const useFetchApplicationStatus = (groupArticleId: number) => {
-  const { data } = useAuthQuery<ApiResponse<{ isJoined: boolean }>, AxiosError, boolean>(
+  const { data } = useAuthQuery<ApiResponse<DataType>, AxiosError, boolean>(
     ['applicationStatus', groupArticleId],
-    () => clientAxios.get(`/v1/group-applications/status/${groupArticleId}`),
+    () => clientAxios.get(`/v1/group-applications/status`, { params: { groupArticleId } }),
     {
       select: (res) => res.data.data.isJoined,
     }
