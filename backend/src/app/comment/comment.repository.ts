414f39b5566d5aa2b509e07ee38583461d512055
pythonstrup <pythonstrup@gmail.com>
2022-12-05 23:28:19 +0900

    ✨ feature: selectAllComment(페이지네이션 X)

--- backend/src/app/comment/comment.repository.ts
+++ backend/src/app/comment/comment.repository.ts
@@ -1,4 +1,4 @@
-import { DataSource, Repository } from 'typeorm';
+import { DataSource, IsNull, Repository } from 'typeorm';
 import { Injectable } from '@nestjs/common';
 import { Comment } from '@app/comment/entity/comment.entity';
 
@@ -10,5 +10,17 @@ export class CommnetRepository extends Repository<Comment> {
       dataSource.createEntityManager(),
       dataSource.createQueryRunner(),
     );
+  }
+
+  selectAllComments(articleId: number) {
+    return this.find({
+      relations: {
+        user: true,
+      },
+      where: {
+        articleId,
+        deletedAt: IsNull(),
+      },
+    });
   }
 }
