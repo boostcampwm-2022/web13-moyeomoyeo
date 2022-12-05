317e6cdd4908e695d1f65863da334bedcda73c1d
pythonstrup <pythonstrup@gmail.com>
2022-12-05 15:46:17 +0900

    🥅 chore: comment에서 필요한 의존성 import해오기

--- backend/src/app/comment/comment.module.ts
+++ backend/src/app/comment/comment.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { CommentController } from '@app/comment/comment.controller';
 import { CommentService } from '@app/comment/comment.service';
 import { CommnetRepository } from '@app/comment/comment.repository';
+import { GroupArticleModule } from '@app/group-article/group-article.module';
 
 @Module({
+  imports: [GroupArticleModule],
   controllers: [CommentController],
   providers: [CommentService, CommnetRepository],
 })
