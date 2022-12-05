b4d75f0e32374c716cacd77fe0b5dd411bdd3c5b
pythonstrup <pythonstrup@gmail.com>
2022-12-05 23:51:00 +0900

    ✨ feature: query dto 작성

---
+++ backend/src/app/comment/dto/get-all-comment-query-request.dto.ts
@@ -0,0 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { PageRequest } from '@src/common/util/page-request';
+import { Type } from 'class-transformer';
+import { IsNumber } from 'class-validator';
+
+export class GetAllCommentQueryRequest extends PageRequest {
+  @Type(() => Number)
+  @IsNumber()
+  @ApiProperty({
+    example: 1,
+    description: '모집 게시글 아이디',
+    required: false,
+  })
+  articleId: number;
+}
