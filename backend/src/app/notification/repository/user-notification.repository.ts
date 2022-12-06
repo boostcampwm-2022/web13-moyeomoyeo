8421fb973234afedda197fcbd6cb7ebb85f43bea
myeongil <username1103@naver.com>
2022-12-06 18:00:51 +0900

    :sparkles: feature: 알림데이터 조회 쿼리 추가

---
+++ backend/src/app/notification/repository/user-notification.repository.ts
@@ -0,0 +1,38 @@
+import { DataSource, IsNull, Repository } from 'typeorm';
+import { UserNotification } from '@app/notification/entity/user-notification.entity';
+import { Injectable } from '@nestjs/common';
+import { User } from '@app/user/entity/user.entity';
+
+@Injectable()
+export class UserNotificationRepository extends Repository<UserNotification> {
+  constructor(private readonly dataSource: DataSource) {
+    const baseRepository = dataSource.getRepository(UserNotification);
+    super(
+      baseRepository.target,
+      baseRepository.manager,
+      baseRepository.queryRunner,
+    );
+  }
+
+  getNotifications({
+    user,
+    limit,
+    offset,
+  }: {
+    user: User;
+    limit: number;
+    offset: number;
+  }) {
+    return this.findAndCount({
+      relations: {
+        notification: true,
+      },
+      where: {
+        userId: user.id,
+        deletedAt: IsNull(),
+      },
+      take: limit,
+      skip: offset,
+    });
+  }
+}
