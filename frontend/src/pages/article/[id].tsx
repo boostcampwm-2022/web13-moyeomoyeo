65ce0627e442eed2efd33406e33e9fcc95452031
Yangseungchan <uvzone@naver.com>
2022-12-06 12:12:03 +0900

    🥅 chore: contents로 나오도록 변경

--- frontend/src/pages/article/[id].tsx
+++ frontend/src/pages/article/[id].tsx
@@ -111,7 +111,7 @@ const ArticleDetail = () => {
                     color={indigo[7]}
                   />
                   <TypographyStylesProvider>
-                    <ContentBox dangerouslySetInnerHTML={{ __html: article.content }} />
+                    <ContentBox dangerouslySetInnerHTML={{ __html: article.contents }} />
                   </TypographyStylesProvider>
                   {article.author.id !== myInfo.id && (
                     <ParticipateButton
