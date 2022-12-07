8607d46456f2a8910c06cb2f5f2411cea117a1ab
pythonstrup <pythonstrup@gmail.com>
2022-12-07 16:37:03 +0900

    🥅 chore: 코드 리뷰 의견에 따른 코드 수정
    - Forbidden은 권한과 관련된 에러이기 때문에 BadRequestException으로 변경

--- backend/src/app/group-application/exception/closed-group.exception.ts
+++ backend/src/app/group-application/exception/closed-group.exception.ts
@@ -1,6 +1,6 @@
-import { ForbiddenException } from '@nestjs/common';
+import { BadRequestException } from '@nestjs/common';
 
-export class ClosedGroupException extends ForbiddenException {
+export class ClosedGroupException extends BadRequestException {
   constructor(message = '모임이 모집완료 혹은 모집중단 상태입니다.') {
     super({ status: 'CLOSED_GROUP', message });
   }
