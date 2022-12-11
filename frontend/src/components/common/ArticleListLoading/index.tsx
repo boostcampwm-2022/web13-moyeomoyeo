12ff70d4cc93a67c97861cd6bef03ad30408fd87
MinGyeong Park <37508296+kong430@users.noreply.github.com>
2022-12-11 14:22:04 +0900

    :sparkles: feature: useDefferedResponse hook 생성 및 skeleton 컴포넌트에 적용 (#278)

--- frontend/src/components/common/ArticleListLoading/index.tsx
+++ frontend/src/components/common/ArticleListLoading/index.tsx
@@ -1,6 +1,14 @@
 import { Skeleton } from '@mantine/core';
 
+import useDeferredResponse from '@hooks/useDeferredResponse';
+
 const ArticleListLoading = () => {
+  const isDeferred = useDeferredResponse();
+
+  if (!isDeferred) {
+    return <></>;
+  }
+
   return (
     <>
       {new Array(8).fill(0).map((_, index) => (
