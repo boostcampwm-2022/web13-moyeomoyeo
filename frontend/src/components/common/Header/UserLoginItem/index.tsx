9fade1f0cc98cd18089c734c062452afdce0bda2
yangseungchan <uvzone@naver.com>
2022-12-12 11:58:38 +0900

    [Feature] 로그아웃 연동 및 추가 이미지 최적화, Favicon + OG 변경 (#286)
    * 🥅 chore: 불필요한 axios 로직 간소화
    
    * ✨ feature: 로그아웃 API 연동
    
    * ✨ feature: 로그아웃 기능 추가
    
    * ✨ feature: favicon 추가 및 적용
    
    * 🥅 chore: lang 속성 추가
    
    * ✨ feature: openGraph 추가
    
    * 🥅 chore: OG이미지 url 변경

--- frontend/src/components/common/Header/UserLoginItem/index.tsx
+++ frontend/src/components/common/Header/UserLoginItem/index.tsx
@@ -1,47 +1,66 @@
 import Link from 'next/link';
+import { useState } from 'react';
 
 import { Menu, Text } from '@mantine/core';
 
+import AlertModal from '@components/common/AlertModal';
 import Avatar from '@components/common/Avatar';
 import LoginButton from '@components/common/Header/LoginButton';
 import useFetchMyInfo from '@hooks/queries/useFetchMyInfo';
-
-/**
- * TODO
- * 로그아웃 API 연동하기
- */
+import useLogout from '@hooks/queries/useLogout';
+import { showToast } from '@utils/toast';
 
 const UserLoginItem = () => {
   const { data: myData, isLoading } = useFetchMyInfo();
+  const { mutate: logout } = useLogout();
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const handleClickLogoutItem = async () => {
+    logout(
+      {},
+      {
+        onSuccess: () => {
+          showToast({ title: '로그아웃 완료!', message: '다음에도 꼭 이용해주세요!' });
+        },
+      }
+    );
+  };
 
   if (isLoading) return null;
 
   return myData ? (
-    <Menu position="bottom-end">
-      <Menu.Target>
-        <Avatar
-          size="md"
-          alt={myData.userName}
-          src={myData.profileImage}
-          style={{ cursor: 'pointer' }}
-          priority
-        />
-      </Menu.Target>
-      <Menu.Dropdown>
-        <Link href="/my">
-          <Menu.Item p="md">
+    <>
+      <AlertModal
+        message="로그아웃 되었습니다."
+        open={modalOpen}
+        onClose={() => setModalOpen(false)}
+      />
+      <Menu position="bottom-end">
+        <Menu.Target>
+          <Avatar
+            size="md"
+            alt={myData.userName}
+            src={myData.profileImage}
+            style={{ cursor: 'pointer' }}
+            priority
+          />
+        </Menu.Target>
+        <Menu.Dropdown>
+          <Link href="/my">
+            <Menu.Item p="md">
+              <Text fz="md" fw={500}>
+                내 프로필
+              </Text>
+            </Menu.Item>
+          </Link>
+          <Menu.Item p="md" onClick={handleClickLogoutItem}>
             <Text fz="md" fw={500}>
-              내 프로필
+              로그아웃
             </Text>
           </Menu.Item>
-        </Link>
-        <Menu.Item p="md">
-          <Text fz="md" fw={500}>
-            로그아웃
-          </Text>
-        </Menu.Item>
-      </Menu.Dropdown>
-    </Menu>
+        </Menu.Dropdown>
+      </Menu>
+    </>
   ) : (
     <LoginButton />
   );
