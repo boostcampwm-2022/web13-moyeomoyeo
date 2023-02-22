01befb1a26dc021ad4832e0d45e2bafcb4cdc87e
Kim Myeongil <mikim961103@gmail.com>
2023-02-23 00:33:45 +0900

    :sparkles: feature: ImageUpload 전략이 추가될 경우 이미지 스토어 생성에서 타입 에러를 반환하도록 수정

--- backend/src/common/module/image/image.module.ts
+++ backend/src/common/module/image/image.module.ts
@@ -1,26 +1,13 @@
 import { DynamicModule, ValueProvider } from '@nestjs/common';
 import { ImageUploadConfiguration } from '@common/module/image/type/image-upload-configuration.interface';
 import { ImageStore } from '@common/module/image/image-store';
-import { S3ImageStore } from '@common/module/image/s3-image-store';
-import { UploadStrategy } from '@common/module/image/enums/upload-strategy.enum';
-import { FileImageStore } from '@common/module/image/file-image-store';
+import { getImageStore } from '@common/module/image/get-image-store';
 
 export class ImageModule {
   static register(config: ImageUploadConfiguration): DynamicModule {
-    let imageStore: ImageStore;
-
-    // TO DO: ImageFactory 로 분리
-    switch (config.strategy) {
-      case UploadStrategy.S3:
-        imageStore = new S3ImageStore(config);
-        break;
-      case UploadStrategy.FILE:
-        imageStore = new FileImageStore(config);
-    }
-
-    const imageStoreProvider: ValueProvider = {
+    const imageStoreProvider: ValueProvider<ImageStore> = {
       provide: ImageStore,
-      useValue: imageStore,
+      useValue: getImageStore(config),
     };
 
     return {
